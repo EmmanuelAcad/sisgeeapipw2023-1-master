5926f0e2181203a8bf7f2e600f77b27249876cbe
EmmanuelAcad <111817257+EmmanuelAcad@users.noreply.github.com>
2023-04-03 22:03:35 -0300

    Add files vias upload

--- routes/rotas.js
+++ routes/rotas.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const { getPredios, addPredio, updatePredio, deletePredio, getPredioPorCodigo } = require('../controllers/prediosController')
+const { getSalas, addSala, updateSala, deleteSala, getSalaPorCodigo } = require('../controllers/salasController')
 
 const rotas = new Router();
 
@@ -12,5 +13,15 @@ rotas.route('/predios')
 rotas.route('/predios/:codigo')
      .get(getPredioPorCodigo)
      .delete(deletePredio);
+
+
+rotas.route('/salas')
+     .get(getSalas)
+     .post(addSala)
+     .put(updateSala);
+
+rotas.route('/salas/:codigo')
+     .get(getSalaPorCodigo)
+     .delete(deleteSala);
 
 module.exports = rotas;
--- useCases/salaUseCases.js
+++ useCases/salaUseCases.js
@@ -26,3 +26,55 @@ const addSalaDB = async (body) => {
         throw "Erro: " + err;
     }
 }
+
+const updateSalaDB = async (body) => {
+    try {
+        const { codigo, numero, descricao, capacidade, predio } = body;
+        const results = await pool.query(`UPDATE salas SET numero = $1, descricao = $2, capacidade = $3, predio = $4
+                                                WHERE codigo = $5
+                                                RETURNING codigo, numero, descricao, capacidade, predio`,
+                                                [numero, descricao, capacidade, predio, codigo]);
+        if (results.rowCount == 0) {
+            throw `Nenhum registro encontrado com o código ${codigo} para ser alterado`;
+        }
+
+        const sala = results.rows[0];
+
+        return new Sala(sala.codigo, sala.numero, sala.descricao, sala.capacidade, sala.predio);
+    } catch (err) {
+        throw "Erro ao inserir a sala: " + err;
+    }
+}
+
+const deleteSalaDB = async (codigo) => {
+    try {
+        const results = await pool.query(`DELETE FROM salas
+                                                WHERE codigo = $1`,
+                                                [codigo]);
+        if (results.rowCount == 0) {
+            throw `Nenhum registro encontrado com o código ${codigo} para ser removido`;
+        }
+
+    } catch (err) {
+        throw "Erro ao remover a sala: " + err;
+    }
+}
+
+const getSalaPorCodigoDB = async (codigo) => {
+    try {
+        const results = await pool.query(`SELECT * FROM salas
+                                                WHERE codigo = $1`,
+                                                [codigo]);
+        if (results.rowCount == 0) {
+            throw `Nenhum registro encontrado com o código ${codigo}`;
+        } else {
+            const sala = results.row[0];
+            return new Sala(sala.codigo, sala.numero, sala.descricao, sala.capacidade, sala.predio)
+        }
+
+    } catch (err) {
+        throw "Erro ao recuperar a sala: " + err;
+    }
+}
+
+module.exports = { getSalasDB, addSalaDB, updateSalaDB, deleteSalaDB, getSalaPorCodigoDB }
